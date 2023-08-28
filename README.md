69d658e63df5ab5cc33d9ae0626cf38d62ec8e4c
Alejandra <alenocot@espol.edu.ec>
2023-08-28 02:07:54 -0500

    cambios front

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Proyecto07
+# CoreApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.1.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,60 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { BlogComponent } from './blog/blog.component';
-import { CartComponent } from './cart/cart.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { ContactComponent } from './contact/contact.component';
-import { ServicesComponent } from './services/services.component';
-import { ShopComponent } from './shop/shop.component';
-import { ThankyouComponent } from './thankyou/thankyou.component';
+
+import { MainComponent } from './pages/main/main.component';
+import { ReportComponent } from './pages/report/report.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenuComponent } from './pages/menu/menu.component';
+
+import {MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { HttpClientModule } from  '@angular/common/http';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+
+const routes: Routes = [
+    {path: "main", component: MainComponent },
+    {path: "about", component: AboutComponent },
+    {path: "report", component: ReportComponent },
+    {path: " ", redirectTo: '/main', pathMatch: 'full' },
+    {path: "**", redirectTo: "main" }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
+    ReportComponent,
     AboutComponent,
-    BlogComponent,
-    CartComponent,
-    CheckoutComponent,
-    ContactComponent,
-    ServicesComponent,
-    ShopComponent,
-    ThankyouComponent
+    MenuComponent    
   ],
+
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    BrowserModule,   
+    BrowserAnimationsModule, 
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatSelectModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
